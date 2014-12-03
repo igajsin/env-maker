28eb06056092592847675b9d3dc09707ccf8e723
Igor Gajsin <igajsin@mirantis.com>
2014-12-03 16:13:07 +0300

    just save result of work

--- env-maker/dialog.scm
+++ env-maker/dialog.scm
@@ -3,18 +3,24 @@
 !#
 
 (use-modules (scsh syntax))
+(use-modules (scsh scsh))
+(use-modules (ice-9 receive))
 
-(define oses )
+(define oses '(("1" . "Ubuntu") ("2" . "CentOS")))
 
-(define (d cmd res)
-  "make menu"
+(define-syntax Rn
+  (syntax-rules ()
+    ((Rn exp exp* ...)
+     (run (exp exp* ...) rd))))
+
+(define-syntax-rule (d cmd cmd* ...)
   (let* ((fl (tmpnam))
-	 (r (begin (run (,cmd) (> 2 ,fl))
+	 (r (begin (run (cmd cmd* ...) (> 2 ,fl))
 		   (run/string (cat ,fl)))))
-    (assoc-ref res r)))
+    r))
 
-(define (set-os2)
-  (d "dialog --cancel-label Quit --menu \"Chose OS:\" 10 30 2 1 Ubuntu 2 CentOS" '(("1" . "Ubuntu") ("2" . "CentOS"))))
+(define (set-os2 os)
+  (assoc-ref os (d dialog --cancel-label Quit --menu "Choose OS" 10 30 2 1 Ubuntu 2 CentOS)))
 
 (define (set-os)
   (let* ((fl (tmpnam))
@@ -25,6 +31,16 @@
     (run (rm ,fl))
     (if (string= os "1") "Ubuntu" "CentOS")))
 
+(define (set-os3 os)
+  (receive (status out err)
+    (run/collecting (1 2)
+		    (dialog --cancel-label Quit --menu "Choose OS" 10 30 2 1 Ubuntu 2 CentOS))
+  (assoc-ref os (port->string err))))
+
 (define (main args)
-  (let ((os (set-os2)))
-    (display (string-append "os is " os))))
+  (let ((os (set-os3 oses)))
+    (display (string-append "os is " os "\n"))))
+
+
+
+
--- env-maker/env-maker.scm
+++ env-maker/env-maker.scm
@@ -4,6 +4,7 @@
 
 (use-modules (ice-9 getopt-long))
 (use-modules (scsh syntax))
+(use-modules (ice-9 regex))
 
 (define option-spec '((version (single-char #\v) (value #f))
 			   (help (single-char #\h) (value #f))
@@ -12,8 +13,21 @@
 			   (test-group (single-char #\t) (value #t))
 			   (node-num (single-char #\n) (value #t))))
 
-(define (help-message)
-  (display "\
+(define oses '(("1" . "Ubuntu") ("2" . "CentOS")))
+(define fuel-main "/home/gajsin/src/fuel-main")
+
+(define-syntax-rule (d cmd cmd* ...)
+  (let* ((fl (tmpnam))
+	 (r (begin (run (cmd cmd* ...) (> 2 ,fl))
+		   (run/string (cat ,fl)))))
+    r))
+
+(define (set-os os)
+  (assoc-ref os (d dialog --cancel-label Quit --menu "Choose OS" 10 30 2 1 Ubuntu 2 CentOS)))
+
+(define (help-message help-wanted)
+  (when help-wanted
+    (display "\
 Usage: env-maker.scm [options]
    -v, --version\tDisplay version
    -h, --help\t\tDisplay this help
@@ -21,14 +35,32 @@ Usage: env-maker.scm [options]
    -i, --iso\t\tPath to fuel master iso
    -t, --test-group\tName of testgroup which will be launched
    -n, --node-num\tNumber of node for creation
-"))
+")
+    (quit)))
 
-(define (version-message) (display "0.1\n"))
+(define (version-message v)
+  (when v (display "0.1\n")
+	(quit)))
 
-(define (set-os)
-  "choose Ubuntu or CentOS"
-  (let ((os  (run/string  (ls /lala) (= 2 1))))
-    os))
+(define (check-iso iso)
+  (when (or (not iso) (not (file-exists? iso)))
+    (d dialog --title "Fuel master iso not found" --msgbox "Please set correct path to fuel master iso" 5 40)
+    (quit)))
+
+(define (find-test-group path)
+  (let* ((test-path (string-append path "/fuelweb_test/tests"))
+	 (fls (run/strings (find ,test-path -type f -name "*.py")))
+	 (s1 (map (lambda (fl) (run/string (grep vcenter ,fl)))  fls))
+	 (s2 s1))
+    (display s2)))
+
+(define (set-test-group path)
+  (when (not (file-exists? path))
+    (let ((error-message (string-append path " not found")))
+      (d dialog --title "Critical error" --msgbox ,error-message 5 40))
+    (quit))
+  (display (find-test-group path)))
+
 
 (define (main args)
   "prepare command line args"
@@ -39,12 +71,11 @@ Usage: env-maker.scm [options]
 	 (iso (option-ref options 'iso #f))
 	 (test-group (option-ref options 'test-group #f))
 	 (node-num (option-ref options 'node-num #f)))
-    (when help-wanted (help-message))
-    (when version-wanted (version-message))
-    (when (not os) (set! os (set-os)))
-    (display os) (newline)
-    (when test-group
-      (display test-group) (newline))
+    (help-message help-wanted)
+    (version-message version-wanted)
+    (check-iso iso)
+    (when (not os) (set! os (set-os oses)))
+    (when (not test-group) (set! test-group (set-test-group fuel-main)))
     (when node-num
       (display node-num) (newline))
     (when os
